40f579d55944c529b2f1147d4758a22b9188cd24
John Pursey <john.pursey@gmail.com>
2018-05-27 11:45:39 -0700

    Initial commit

---
+++ Gridifier/MainForm.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Gridifier
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileSaveAs = new System.Windows.Forms.ToolStripMenuItem();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileMenu});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(1298, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip1";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileOpen,
+            this.fileSave,
+            this.fileSaveAs});
+            this.fileMenu.Name = "fileMenu";
+            this.fileMenu.Size = new System.Drawing.Size(37, 20);
+            this.fileMenu.Text = "File";
+            // 
+            // fileOpen
+            // 
+            this.fileOpen.Name = "fileOpen";
+            this.fileOpen.Size = new System.Drawing.Size(123, 22);
+            this.fileOpen.Text = "Open...";
+            this.fileOpen.Click += new System.EventHandler(this.fileOpen_Click);
+            // 
+            // fileSave
+            // 
+            this.fileSave.Name = "fileSave";
+            this.fileSave.Size = new System.Drawing.Size(123, 22);
+            this.fileSave.Text = "Save";
+            this.fileSave.Click += new System.EventHandler(this.fileSave_Click);
+            // 
+            // fileSaveAs
+            // 
+            this.fileSaveAs.Name = "fileSaveAs";
+            this.fileSaveAs.Size = new System.Drawing.Size(123, 22);
+            this.fileSaveAs.Text = "Save As...";
+            this.fileSaveAs.Click += new System.EventHandler(this.fileSaveAs_Click);
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackColor = System.Drawing.Color.DimGray;
+            this.imageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox.ImageLocation = "";
+            this.imageBox.Location = new System.Drawing.Point(0, 24);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(1298, 859);
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            this.imageBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.imageBox_MouseClick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 883);
+            this.Controls.Add(this.imageBox);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "Gridifier";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileMenu;
+        private System.Windows.Forms.ToolStripMenuItem fileOpen;
+        private System.Windows.Forms.ToolStripMenuItem fileSave;
+        private System.Windows.Forms.PictureBox imageBox;
+        private System.Windows.Forms.ToolStripMenuItem fileSaveAs;
+    }
+}
+
---
+++ Gridifier/MainForm.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gridifier
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        // Functions
+
+        void UpdateImage()
+        {
+            if (m_sourceImage == null)
+                return;
+
+            var graphics = Graphics.FromImage(m_editImage);
+            graphics.DrawImageUnscaled(m_sourceImage, Point.Empty);
+
+            Point start = new Point(m_start.X, m_start.Y);
+            Point end = new Point(m_start.X, m_end.Y);
+            int width = m_end.X - m_start.X;
+            int height = m_end.Y - m_start.Y;
+
+            for (int i = 0; i <= m_gridXCount; ++i)
+            {
+                start.X = end.X = m_start.X + (i * width / m_gridXCount);
+                graphics.DrawLine(Pens.Yellow, start, end);
+            }
+
+            start.X = m_start.X;
+            end.X = m_end.X;
+
+            for (int i = 0; i <= m_gridYCount; ++i)
+            {
+                start.Y = end.Y = m_start.Y + (i * height / m_gridYCount);
+                graphics.DrawLine(Pens.Yellow, start, end);
+            }
+
+            imageBox.Image = m_editImage;
+
+            Text = String.Format("Gridifier ({0}x{1}) - {2} pixels",
+                m_gridXCount, m_gridYCount, (m_end.X - m_start.X) / m_gridXCount);
+        }
+
+        private void Save(String filename)
+        {
+            var width = m_end.X - m_start.X + 1;
+            var height = m_end.Y - m_start.Y + 1;
+            Rectangle dstRect = new Rectangle(0, 0, width, height);
+            Rectangle srcRect = new Rectangle(m_start.X, m_start.Y, width, height);
+
+            var newImage = new Bitmap(width, height);
+            var graphics = Graphics.FromImage(newImage);
+            graphics.DrawImage(m_sourceImage, dstRect, srcRect, GraphicsUnit.Pixel);
+            graphics.Save();
+
+            newImage.Save(filename);
+
+            m_sourceImage = newImage;
+            m_editImage = m_sourceImage.Clone() as Image;
+            m_filename = filename;
+            m_end.X -= m_start.X;
+            m_end.Y -= m_start.Y;
+            m_start = Point.Empty;
+            UpdateImage();
+        }
+
+        // Data
+        private String m_filename;
+        private Image m_sourceImage;
+        private Image m_editImage;
+        private Point m_start;
+        private Point m_end;
+        private int m_gridXCount = 1;
+        private int m_gridYCount = 1;
+
+        // Events
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void fileOpen_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Title = "Open Image";
+            ofd.Multiselect = false;
+            ofd.Filter = "Image files (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*jpeg;*.png;*.bmp|All files (*.*)|*.*";
+            ofd.FilterIndex = 1;
+            if (ofd.ShowDialog() != DialogResult.OK)
+                return;
+
+            m_filename = ofd.FileName;
+            m_sourceImage = Image.FromFile(m_filename);
+            m_editImage = m_sourceImage.Clone() as Image;
+            m_start.X = 0;
+            m_start.Y = 0;
+            m_end.X = m_sourceImage.Width - 1;
+            m_end.Y = m_sourceImage.Height - 1;
+            m_gridXCount = m_gridYCount = 1;
+            UpdateImage();
+        }
+
+        private void fileSave_Click(object sender, EventArgs e)
+        {
+            Save(m_filename);
+        }
+
+        private void fileSaveAs_Click(object sender, EventArgs e)
+        {
+            var sfd = new SaveFileDialog();
+            sfd.AddExtension = true;
+            sfd.FileName = m_filename;
+            sfd.Title = "Save Image As";
+            sfd.Filter = "Image files (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*jpeg;*.png;*.bmp|All files (*.*)|*.*";
+            sfd.FilterIndex = 1;
+            if (sfd.ShowDialog() != DialogResult.OK)
+                return;
+
+            Save(sfd.FileName);
+        }
+
+        private void imageBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (m_sourceImage == null || e.Location.X >= m_editImage.Size.Width || e.Location.Y >= m_editImage.Size.Height)
+                return;
+            if (e.Button == MouseButtons.Left)
+            {
+                if (e.Location.X >= m_end.X || e.Location.Y >= m_end.Y)
+                    return;
+                m_start = e.Location;
+                UpdateImage();
+            }
+            else if (e.Button == MouseButtons.Right)
+            {
+                if (e.Location.X <= m_start.X || e.Location.Y <= m_start.Y)
+                    return;
+                m_end = e.Location;
+                UpdateImage();
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (m_sourceImage == null)
+                return;
+            if (e.KeyCode == Keys.Add)
+            {
+                if (e.Shift)
+                    m_gridYCount += 1;
+                if (!e.Shift)
+                    m_gridXCount += 1;
+
+                UpdateImage();
+            }
+            if (e.KeyCode == Keys.Subtract)
+            {
+                if (e.Shift && m_gridYCount > 1)
+                    m_gridYCount -= 1;
+                if (!e.Shift && m_gridXCount > 1)
+                    m_gridXCount -= 1;
+                UpdateImage();
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                if (e.Alt)
+                {
+                    int gridY = (m_end.Y - m_start.Y) / m_gridYCount;
+                    if (e.Shift && m_gridYCount > 1)
+                    {
+                        m_end.Y -= gridY;
+                        --m_gridYCount;
+                    }
+                    if (!e.Shift && m_start.Y >= gridY)
+                    {
+                        m_start.Y -= gridY;
+                        ++m_gridYCount;
+                    }
+                }
+                else
+                {
+                    if (e.Shift && m_end.Y > m_start.Y + 1)
+                        m_end.Y -= 1;
+                    if (!e.Shift && m_start.Y > 0)
+                        m_start.Y -= 1;
+                }
+                UpdateImage();
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (e.Alt)
+                {
+                    int gridY = (m_end.Y - m_start.Y) / m_gridYCount;
+                    if (e.Shift && m_end.Y < m_editImage.Height - gridY)
+                    {
+                        m_end.Y += gridY;
+                        ++m_gridYCount;
+                    }
+                    if (!e.Shift && m_gridYCount > 1)
+                    {
+                        m_start.Y += gridY;
+                        --m_gridYCount;
+                    }
+                }
+                else
+                {
+                    if (e.Shift && m_end.Y < m_editImage.Height - 1)
+                        m_end.Y += 1;
+                    if (!e.Shift && m_start.Y < m_end.Y - 1)
+                        m_start.Y += 1;
+                }
+                UpdateImage();
+            }
+            if (e.KeyCode == Keys.Left)
+            {
+                if (e.Alt)
+                {
+                    int gridX = (m_end.X - m_start.X) / m_gridXCount;
+                    if (e.Shift && m_gridXCount > 1)
+                    {
+                        m_end.X -= gridX;
+                        --m_gridXCount;
+                    }
+                    if (!e.Shift && m_start.X >= gridX)
+                    {
+                        m_start.X -= gridX;
+                        ++m_gridXCount;
+                    }
+                }
+                else
+                {
+                    if (e.Shift && m_end.X > m_start.X + 1)
+                        m_end.X -= 1;
+                    if (!e.Shift && m_start.X > 0)
+                        m_start.X -= 1;
+                }
+                UpdateImage();
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                if (e.Alt)
+                {
+                    int gridX = (m_end.X - m_start.X) / m_gridXCount;
+                    if (e.Shift && m_end.X < m_editImage.Width - gridX)
+                    {
+                        m_end.X += gridX;
+                        ++m_gridXCount;
+                    }
+                    if (!e.Shift && m_gridXCount > 1)
+                    {
+                        m_start.X += gridX;
+                        --m_gridXCount;
+                    }
+                }
+                else
+                {
+                    if (e.Shift && m_end.X < m_editImage.Width - 1)
+                        m_end.X += 1;
+                    if (!e.Shift && m_start.X < m_end.X - 1)
+                        m_start.X += 1;
+                }
+                UpdateImage();
+            }
+        }
+    }
+}
